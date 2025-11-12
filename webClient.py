129e3eaf20965785e01b2106b2dc9dd716a8e79f
Your Full Name <your.email@student.depaul.edu>
2025-11-11 20:30:32 -0600

    end class content

--- webClient.py
+++ webClient.py
@@ -25,9 +25,57 @@ class LinkParser(HTMLParser):
                     print(attr[1])
 
 
-afile = open('links.html')
-contents = afile.read()
-afile.close()
+def linkParser():
+    afile = open('links.html')
+    contents = afile.read()
+    afile.close()
 
-lp = LinkParser()
-lp.feed(contents)
+    lp = LinkParser()
+    lp.feed(contents)
+
+
+class MyHTMLParser(HTMLParser):
+
+    def __init__(self, url):
+        if 'http' in url or 'https' in url:
+            resp = urllib.request.urlopen(url)
+            content = resp.read()
+            html = content.decode().lower() 
+            self.feed(html)
+        else:
+            afile = open('links.html')
+            file_contents = afile.read()
+            afile.close()
+            self.feed(file_contents)
+
+    def handle_starttag(self, tag, attrs):
+        
+        if tag == 'html':
+            print('{} start'.format(tag))
+        elif tag == 'head' or tag == 'body':
+            print ('\t{} start'.format(tag))
+        elif tag == 'title' or tag == 'h1' or tag == 'h2' or tag == 'a' or tag == 'ol':
+            print('\t\t{} start'.format(tag))
+        elif tag == 'li':
+            print('\t\t\t{} start'.format(tag))
+        
+    def handle_endtag(self, tag):
+        if tag == 'html':
+            print('{} end'.format(tag))
+        elif tag == 'head' or tag == 'body':
+            print ('\t{} end'.format(tag))
+        elif tag == 'title' or tag == 'h1' or tag == 'h2' or tag == 'a' or tag == 'ol':
+            print('\t\t{} end'.format(tag))
+        elif tag == 'li':
+            print('\t\t\t{} end'.format(tag))
+    
+
+# def myParser():
+#     afile = open('index.html')
+#     contents = afile.read()
+#     afile.close()
+
+#     mp = MyHTMLParser()
+#     mp.feed(contents)
+
+mp = MyHTMLParser('https://w3.org')
