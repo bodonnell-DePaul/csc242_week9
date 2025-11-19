9426252bdf30778964e2e85898d4ce557138d73a
Your Full Name <your.email@student.depaul.edu>
2025-11-18 19:18:49 -0600

    new baseball parser

--- webClient.py
+++ webClient.py
@@ -1,4 +1,5 @@
 import urllib.request
+from html.parser import HTMLParser
 
 # t = urllib.request.urlopen('https://www.w3.org/')
 # t.geturl()
@@ -14,7 +15,7 @@ def news(url, topics):
 
 #news('https://bbc.co.uk', ['news', 'business', 'travel'])
 
-from html.parser import HTMLParser
+
 class LinkParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         'print value of href attribute if any'
@@ -69,14 +70,129 @@ class MyHTMLParser(HTMLParser):
             print('\t\t{} end'.format(tag))
         elif tag == 'li':
             print('\t\t\t{} end'.format(tag))
+
+class MLBGameList(HTMLParser):
+    def __init__(self,url):
+        super().__init__()
+        self.url = url[8:]
+        self.url = self.url.split('/')[0]
+        self.links = []
+        self.isLink = False
+        self.lastLink = ''
+        if 'http' in url or 'https' in url:
+            resp = urllib.request.urlopen(url)
+            content = resp.read()
+            html = content.decode()
+            self.feed(html)
+        pass
+
+    def handle_starttag(self, tag, attrs):
+        super().handle_starttag(tag, attrs)
+        if tag == 'a':
+            for attr in attrs:
+                if attr[0] == 'href':
+                    #a trigger for our handle_data function
+                    self.isLink = True
+                    #gathering the address and storing it in a variable
+                    self.lastLink = attr[1]
+
+
+
+    def handle_data(self, data):
+        super().handle_data(data)
+        if self.isLink is True:
+            #detecting if we want to add the url to our list
+            if data.lower() == 'boxscore':
+                self.links.append('https://'+self.url+self.lastLink)
+
+
+
+    def handle_endtag(self, tag):
+        return super().handle_endtag(tag)
     
 
-# def myParser():
-#     afile = open('index.html')
-#     contents = afile.read()
-#     afile.close()
+class DataGather(HTMLParser):
+    def __init__(self, url):
+        super().__init__()
+        self.boxScore = []
+        self.tableHeader = []
+        self.teamOneTable = []
+        self.teamTwoTable = []
+        self.gatherData = False
+        self.gatherHeader = False
+        self.gatherTeamOne = False
+        self.gatherTeamTwo = False
+        if 'http' in url or 'https' in url:
+            resp = urllib.request.urlopen(url)
+            content = resp.read()
+            html = content.decode()
+            self.feed(html)
+        pass
 
-#     mp = MyHTMLParser()
-#     mp.feed(contents)
+    def handle_starttag(self, tag, attrs):
+        super().handle_starttag(tag, attrs)
+        if tag == 'div':
+            for attr in attrs:
+                if attr[0] == 'class' and attr[1] == 'linescore_wrap':
+                    self.gatherData = True
+        if tag == 'thead':
+            self.gatherHeader = True
+        if tag == 'tbody':
+            self.gatherTeamOne = True
+            #self.gatherTeamTwo = True
 
-mp = MyHTMLParser('https://w3.org')
+
+
+    def handle_data(self, data):
+        super().handle_data(data)
+        if self.gatherData is True:
+            if self.gatherHeader is True and data.find('\n') == -1:
+                self.tableHeader.append(data)
+            elif self.gatherTeamOne is True and data.find('\n') == -1:
+                self.teamOneTable.append(data)
+            elif self.gatherTeamTwo is True and data.find('\n') == -1:
+                self.teamTwoTable.append(data)
+
+
+
+
+    def handle_endtag(self, tag):
+        super().handle_endtag(tag)
+        if tag == 'thead':
+            if self.gatherHeader is True:
+                self.gatherHeader = False
+                print(self.tableHeader)
+        if tag == 'tr':
+            if self.gatherTeamOne is True:
+                self.gatherTeamOne = False
+                self.gatherTeamTwo = True
+                while len(self.teamOneTable) > len(self.tableHeader):
+                    self.teamOneTable.pop(0)
+                
+                self.teamOneTable[0] = '\xa0'
+                self.teamOneTable[1] = '\xa0'
+
+                print(self.teamOneTable)
+
+            elif self.gatherTeamTwo is True:
+                self.gatherTeamTwo = False
+                while len(self.teamTwoTable) > len(self.tableHeader):
+                    self.teamTwoTable.pop(0)
+                
+                self.teamTwoTable[0] = '\xa0'
+                self.teamTwoTable[1] = '\xa0'
+
+                print(self.teamTwoTable)
+
+            if self.gatherTeamOne is False and self.gatherTeamTwo is False:
+                self.boxScore.append(self.tableHeader)
+                self.boxScore.append(self.teamOneTable)
+                self.boxScore.append(self.teamTwoTable)
+
+                print(self.boxScore)
+
+
+
+baseballSchedule = "https://www.baseball-reference.com/leagues/majors/2025-schedule.shtml"
+baseballLinks = MLBGameList(baseballSchedule)
+gameData = DataGather(baseballLinks.links[0])
